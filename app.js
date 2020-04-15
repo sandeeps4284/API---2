a9f06d29b826235ca0e02bb49928ded10c4a7962
sandeeps4284 <sandeeps4284@gmail.com>
2020-04-15 17:53:22 +0530

    Third Commit

--- app.js
+++ app.js
@@ -1,19 +1,67 @@
-document.getElementById('button').addEventListener('click',loadData);
+document.getElementById('button1').addEventListener('click',loadCustomer);
+
+document.getElementById('button2').addEventListener('click',loadCustomers);
 
 
-function loadData(){
+//load single customer
 
+function loadCustomer(e){
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET','data.txt',true);
-
-    //console.log('Readystate',xhr.readyState);
+    xhr.open('GET','customer.json',true);
 
     xhr.onload = function(){
-        if(this.status === 200) {
-            console.log(this.responseText);
+        if(this.status === 200){
+            //console.log(this.responseText);
+
+            const customer = JSON.parse(this.responseText);
+
+            const output = `
+            <ul>
+                <li>ID: ${customer.id}</li>
+                <li>ID: ${customer.name}</li>
+                <li>ID: ${customer.company}</li>
+                <li>ID: ${customer.phone}</li>
+            </ul>
+            `;
+
+            document.getElementById('customer').innerHTML = output;
         }
     }
 
-    xhr.send()
+    xhr.send();
+}
+
+//load customers
+
+function loadCustomers(e){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET','customers.json',true);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            //console.log(this.responseText);
+
+            const customers = JSON.parse(this.responseText);
+
+            let output = '';
+
+            customers.forEach(function(customer){
+                output +=`
+                <ul>
+                <li>ID: ${customer.id}</li>
+                <li>ID: ${customer.name}</li>
+                <li>ID: ${customer.company}</li>
+                <li>ID: ${customer.phone}</li>
+                </ul>
+              `;    
+            });  
+            
+
+            document.getElementById('customers').innerHTML = output;
+        }
+    }
+
+    xhr.send();
 }
